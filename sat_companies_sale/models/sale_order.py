5a7a538ade7072777db0c542386444b1185a3365
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-02-28 09:44:27 -0500

    Update sale_order.py

--- sat_companies_sale/models/sale_order.py
+++ sat_companies_sale/models/sale_order.py
@@ -189,3 +189,34 @@ class SaleOrder(models.Model):
             else:
                 print("Presupuestos sin enviar...!")
                 record.quote_date_sent = False
+
+
+    def action_send_email(self):
+        self.ensure_one()
+        ir_model_data = self.env['ir.model.data']
+        try:
+            template_id = \
+            ir_model_data.get_object_reference('test_email', 'email_template')[1]
+        except ValueError:
+            template_id = False
+        try:
+            compose_form_id = ir_model_data.get_object_reference('mail', 'email_compose_message_wizard_form')[1]
+        except ValueError:
+            compose_form_id = False
+        ctx = {
+        'default_model': 'test.email',
+        'default_res_id': self.ids[0],
+        'default_use_template': bool(template_id),
+        'default_template_id': template_id,
+        'default_composition_mode': 'comment',
+        }
+        return {
+        'name': _('Compose Email'),
+        'type': 'ir.actions.act_window',
+        'view_mode': 'form',
+        'res_model': 'mail.compose.message',
+        'views': [(compose_form_id, 'form')],
+        'view_id': compose_form_id,
+        'target': 'new',
+        'context': ctx,
+        }
