5d737df19faf0aa32ed9d50484665d5259fd82a1
bitsluispaternina <53566346+luisfpaternina@users.noreply.github.com>
2022-03-01 09:57:17 -0500

    Update res_company.py

--- sat_companies_sale_suscription/models/res_company.py
+++ sat_companies_sale_suscription/models/res_company.py
@@ -2,3 +2,7 @@ from odoo import models, fields, api, _
 
 class ResCompany(models.Model):
     _inherit = 'res.company'
+
+    partner_manager_id = fields.Many2one(
+        'res.partner',
+        string="Partner manager")
